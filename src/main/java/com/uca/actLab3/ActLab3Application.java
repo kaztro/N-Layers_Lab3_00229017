f54c743daab6397f0f840499990c4d6d8bfb5a47
kaztro <00229017@uca.edu.sv>
2020-05-05 01:12:16 -0600

    Fin.-

---
+++ src/main/java/com/uca/actLab3/ActLab3Application.java
@@ -0,0 +1,13 @@
+package com.uca.actLab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActLab3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/actLab3/ActLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.actLab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActLab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
